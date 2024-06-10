b6a14816ea5681515a50aa789fb0206d2e160a21
Fatma <fatmajrad2000@gmail.com>
2024-06-10 15:14:45 +0100

    routes configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import './App.css';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
-
   return (
-    <div className="App">
-      <h1 className='text-2xl'>Amazing Fatma !!!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} /> 
+        <Route path="/offers" element={<Offers />} /> 
+        <Route path="/profile" element={<Profile />} />      
+        <Route path="/signIn" element={<SignIn />} />   
+        <Route path="/signUp" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
