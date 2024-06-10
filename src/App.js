1e30f8ef6bff0f632734a7b8dd63e75c4c9e589a
Fatma <fatmajrad2000@gmail.com>
2024-06-10 13:33:37 +0100

    project creation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-2xl'>Amazing Fatma !!!</h1>
+    </div>
+  );
+}
+
+export default App;
